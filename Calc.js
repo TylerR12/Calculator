2392527641f88e8eab3dad56b7f64ed416c108c6
TylerR12 <71300978+TylerR12@users.noreply.github.com>
2022-03-05 19:44:48 -0500

    uploading calculator to github

---
+++ Calc.js
@@ -0,0 +1,30 @@
+         //function that display value
+         function dis(val)
+         {
+             document.getElementById("result").value+=val
+         }
+           
+         //function that evaluates the digit and return result
+         function solve()
+         {
+             let x = document.getElementById("result").value
+             let y = eval(x)
+             document.getElementById("result").value = y
+         }
+           
+         //function that clear the display
+         function clr()
+         {
+             document.getElementById("result").value = ""
+         }
+
+
+         function show(){
+             var show = document.getElementById("calcs");
+             show.style.display = "flex";
+         }
+
+         function hide(){
+            var hide = document.getElementById("calcs");
+            hide.style.display = "none";
+         }
